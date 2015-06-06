c27d36488445ad41e04e78be6d6af7c4cf1fc6b9
Your Name <you@example.com>
2015-06-06 10:44:42 +0800

    create container via create api.

---
+++ AgentHost/containerController.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+	"fmt"
+	// "bytes"
+	"strings"
+	"net/http"
+	"encoding/json"
+	"github.com/fsouza/go-dockerclient"
+)
+
+func getPostData(r *http.Request) map[string]string {
+	postData := make(map[string]string)
+	if "POST" == r.Method {
+		r.ParseForm()
+		fmt.Println(r.PostForm)
+		for k, v := range r.PostForm {
+			postData[string(k)] = strings.Join(v,",")
+		}
+	}
+	fmt.Println(postData)
+	return postData
+}
+
+func createContainer(w http.ResponseWriter, r *http.Request) string {
+	data := getPostData(r)
+	client, _ := docker.NewClient("unix:///var/run/docker.sock")
+	config := &docker.Config{
+            Image: data["image"],
+    }
+    containerOptions := docker.CreateContainerOptions{
+        Name: data["name"],
+        Config: config,
+        HostConfig: nil,
+    }
+    exec, err := client.CreateContainer(containerOptions)
+    s, _ := json.Marshal(exec)
+	if err != nil {
+		fmt.Println(err)
+	}
+	return string(s)
+}
