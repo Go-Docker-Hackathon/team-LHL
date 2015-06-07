663882e84600c47d1991a9fb391f3cb039788aa7
Your Name <you@example.com>
2015-06-07 08:12:54 +0800

    test get tags when given agent id.

--- AgentHost/resource/resource_test.go
+++ AgentHost/resource/resource_test.go
@@ -11,4 +11,9 @@ var _ = Describe("Resource", func() {
 			Expect(GetImage([]string{"ruby"})).To(Equal("0c689384ea3b"))
 		})
 	})
+	Describe("test get image tags", func(){
+		It("should get ruby when resource is 0c689384ea3b", func(){
+			Expect(GetImageTags("0c689384ea3b")).To(Equal([]string{"ruby"}))
+		})
+	})
 })
